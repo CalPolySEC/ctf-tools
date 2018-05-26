8cd8fbab571c6c06346915639f736e192451ed10
Jon Prentice <jon77p@gmail.com>
2018-05-26 14:43:01 -0700

    Created APT and Homebrew installers for some ctf tools

---
+++ aptInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# An APT Installer of helpful tools
+
+sudo apt-get install --quiet nmap wireshark radare2 hashcat
---
+++ brewInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A Homebrew Installer of helpful tools
+
+brew install nmap wireshark radare2 hashcat
